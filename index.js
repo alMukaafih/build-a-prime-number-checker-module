2b012451156688bbbb1dcd28c9420b94d4aa026a
alMukaafih <almukaafih@gmail.com>
2026-09-01 19:28:38 +0100

    feat: completed project

---
+++ index.js
@@ -0,0 +1,11 @@
+function isPrime(/** @type {number} */ n) {
+    if (n === 1) return false 
+
+    for(let i = 2; i < n; i++) {
+        if (n % i === 0) return false
+    }
+
+    return true
+}
+
+module.exports = { isPrime }
